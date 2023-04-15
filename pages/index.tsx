3a93d20bc880edc8efb104130fb4e65aa0ffebbb
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2023-04-16 00:10:08 +0200

    feat: add descriptions and contributors

--- pages/index.tsx
+++ pages/index.tsx
@@ -35,14 +35,36 @@ export default function Home() {
         RetroPGF
       </div>
 
-      <div className="flex flex-wrap gap-y-12">
+      <div className="flex flex-col gap-y-12">
         {(zuzaluCerts as Cert[]).map((cert) => (
-          <Image
-            key={cert.description}
-            className="w-1/3"
-            src={cert.image}
-            alt=""
-          />
+          <div className="flex">
+            <Image
+              key={cert.description}
+              className="w-1/3"
+              src={cert.image}
+              alt=""
+              width={150}
+              height={300}
+            />
+            <div className="flex flex-col gap-y-16 w-2/3 text-white">
+              <div className="flex flex-col">
+                <span className="px-2 my-4 text-2xl font-extrabold text-white bg-rose-500 rounded">
+                  description
+                </span>
+                <span>{cert.description}</span>
+              </div>
+              <div className="flex flex-col">
+                <span className="px-2 my-4 text-2xl font-extrabold text-white bg-rose-500 rounded">
+                  contributors
+                </span>
+                <ul className="list-disc list-inside">
+                  {cert.hypercert.contributors.value.map((contributor) => (
+                    <li>{contributor}</li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          </div>
         ))}
       </div>
     </main>
