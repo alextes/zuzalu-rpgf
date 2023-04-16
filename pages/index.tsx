9e8fc1a3cd644a08c6096e3626917e0432b7ba2c
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2023-04-16 14:10:07 +0200

    feat: mobile update

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,7 +24,7 @@ type Cert = {
 
 export default function Home() {
   return (
-    <main className="flex flex-col justify-between items-center p-24 min-h-screen bg-gray-900">
+    <main className="flex flex-col justify-between items-center p-4 min-h-screen bg-gray-900 lg:p-24">
       <div className="z-10 justify-between items-center w-full max-w-5xl font-mono text-sm lg:flex">
         <div className="flex justify-center w-full">
           <Image
@@ -35,34 +35,35 @@ export default function Home() {
           />
         </div>
       </div>
-      <div className="py-8 mb-16 font-mono text-8xl text-transparent bg-clip-text bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500">
+      <div className="py-8 mb-16 font-mono text-4xl text-transparent bg-clip-text bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 lg:text-8xl">
         RetroPGF
       </div>
 
-      <div className="flex flex-col gap-y-12">
+      <div className="flex flex-col gap-y-32 w-full">
         {(decodedZuzaluCerts as Cert[]).map((cert) => (
-          <div className="flex" key={cert.description}>
-            <Image
-              className="w-1/3"
-              src={cert.image}
-              alt=""
-              width={150}
-              height={300}
-            />
-            <div className="flex flex-col gap-y-16 w-2/3 text-white">
+          <div
+            className="flex flex-col pb-24 border-b-2 border-rose-500 md:flex-row"
+            key={cert.description}
+          >
+            <div className="relative mb-16 w-full h-64">
+              <Image className="object-contain" fill src={cert.image} alt="" />
+            </div>
+            <div className="flex flex-col gap-y-16 w-full text-white">
               <div className="flex flex-col">
-                <span className="px-2 my-4 text-2xl font-extrabold text-white bg-rose-500 rounded">
+                <span className="px-2 mb-4 text-2xl font-extrabold text-white bg-rose-500 rounded">
                   description
                 </span>
                 <span>{cert.description}</span>
               </div>
               <div className="flex flex-col">
-                <span className="px-2 my-4 text-2xl font-extrabold text-white bg-rose-500 rounded">
+                <span className="px-2 mb-4 text-2xl font-extrabold text-white bg-rose-500 rounded">
                   contributors
                 </span>
                 <ul className="list-disc list-inside">
                   {cert.hypercert.contributors.value.map((contributor) => (
-                    <li key={contributor}>{contributor}</li>
+                    <li className="truncate" key={contributor}>
+                      {contributor}
+                    </li>
                   ))}
                 </ul>
               </div>
